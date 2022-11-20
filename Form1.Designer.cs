afb4cdb46e70320b46b8269b05e0755bcf527586
BreGernon <117558964+BreGernon@users.noreply.github.com>
2022-11-19 22:20:09 -0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Activity6
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.convertButton = new System.Windows.Forms.Button();
+            this.gramsBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cupsBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Impact", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(65, 43);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(525, 75);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cooking Companion";
+            // 
+            // convertButton
+            // 
+            this.convertButton.Location = new System.Drawing.Point(534, 235);
+            this.convertButton.Name = "convertButton";
+            this.convertButton.Size = new System.Drawing.Size(162, 58);
+            this.convertButton.TabIndex = 1;
+            this.convertButton.Text = "Convert";
+            this.convertButton.UseVisualStyleBackColor = true;
+            this.convertButton.Click += new System.EventHandler(this.convertButton_Click);
+            // 
+            // gramsBox
+            // 
+            this.gramsBox.Font = new System.Drawing.Font("Impact", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.gramsBox.Location = new System.Drawing.Point(65, 235);
+            this.gramsBox.Name = "gramsBox";
+            this.gramsBox.PlaceholderText = "Measurement in Grams.....";
+            this.gramsBox.Size = new System.Drawing.Size(435, 56);
+            this.gramsBox.TabIndex = 2;
+            this.gramsBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Impact", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(65, 168);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(487, 45);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Enter a measurement in grams:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Impact", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(65, 356);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(353, 45);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Measurement in cups:";
+            // 
+            // cupsBox
+            // 
+            this.cupsBox.Font = new System.Drawing.Font("Impact", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.cupsBox.Location = new System.Drawing.Point(65, 430);
+            this.cupsBox.Name = "cupsBox";
+            this.cupsBox.PlaceholderText = "Measurement in Cups";
+            this.cupsBox.Size = new System.Drawing.Size(435, 56);
+            this.cupsBox.TabIndex = 5;
+            this.cupsBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 37F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.BackgroundImage = global::Activity6.Properties.Resources.istockphoto_1139597769_612x612;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1262, 673);
+            this.Controls.Add(this.cupsBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.gramsBox);
+            this.Controls.Add(this.convertButton);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Impact", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.Name = "Form1";
+            this.Text = "Cooking Companion";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Button convertButton;
+        private TextBox gramsBox;
+        private Label label2;
+        private Label label3;
+        private TextBox cupsBox;
+    }
+}
